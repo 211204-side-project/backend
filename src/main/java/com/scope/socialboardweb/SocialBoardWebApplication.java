b7b68f5a0828b90fae2523090ee556985ff545ee
TaegyunWoo <dnxprbs@gmail.com>
2021-12-07 18:35:54 +0900

    Init Project

---
+++ src/main/java/com/scope/socialboardweb/SocialBoardWebApplication.java
@@ -0,0 +1,13 @@
+package com.scope.socialboardweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBoardWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBoardWebApplication.class, args);
+	}
+
+}
