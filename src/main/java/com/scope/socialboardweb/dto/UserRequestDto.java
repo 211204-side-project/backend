2720ddf0f17f200d78dda8ab0925d148de1d9a2f
9sanha <64877575+9sanha@users.noreply.github.com>
2021-12-08 19:33:24 +0900

    Feat<User>: 필요한 User 관련 클래스 틀 추가

---
+++ src/main/java/com/scope/socialboardweb/dto/UserRequestDto.java
@@ -0,0 +1,4 @@
+package com.scope.socialboardweb.dto;
+
+public class UserRequestDto {
+}
---
+++ src/main/java/com/scope/socialboardweb/service/UserService.java
@@ -0,0 +1,7 @@
+package com.scope.socialboardweb.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
