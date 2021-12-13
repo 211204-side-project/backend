fdc0c92dadbf211aedb5537084e92cd3c14e9c55
9sanha <64877575+9sanha@users.noreply.github.com>
2021-12-14 00:50:48 +0900

    Feat<Post>: Dto 생성

---
+++ src/main/java/com/scope/socialboardweb/domain/Post.java
@@ -0,0 +1,20 @@
+package com.scope.socialboardweb.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+public class Post {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String title;
+    private String Content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/com/scope/socialboardweb/dto/CommentRequestDto.java
@@ -0,0 +1,12 @@
+package com.scope.socialboardweb.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class CommentRequestDto {
+    String comment;
+}
