a4bb25f0c3c8ffaa8dec12b6b5d45f93ebcbdf1c
9sanha <64877575+9sanha@users.noreply.github.com>
2021-12-13 12:51:09 +0900

    Feat<User>: 필요한 User 관련 클래스 틀 추가

--- src/main/java/com/scope/socialboardweb/domain/User.java
+++ src/main/java/com/scope/socialboardweb/domain/User.java
@@ -3,6 +3,7 @@ package com.scope.socialboardweb.domain;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Entity
@@ -12,7 +13,15 @@ public class User {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    private String userId;
 
+    private String nickname;
 
+    private String password;
+
+    // int? string?
+    private String phoneNumber;
+
+    private Boolean isVerifiedEmail;
 
 }
