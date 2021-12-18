30adcc3210caf6914f56e3442b2ccca2bb479128
9sanha <64877575+9sanha@users.noreply.github.com>
2021-12-18 23:55:22 +0900

    <swagger> 참고용 swagger 추가

--- src/main/java/com/scope/socialboardweb/controller/UserAuthController.java
+++ src/main/java/com/scope/socialboardweb/controller/UserAuthController.java
@@ -4,23 +4,30 @@ import com.scope.socialboardweb.dto.AuthNicknameDto;
 import com.scope.socialboardweb.dto.AuthPhoneNumberDto;
 import com.scope.socialboardweb.dto.AuthUserIdDto;
 import com.scope.socialboardweb.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "UserAuthController",description = "중복 체크")
 public class UserAuthController {
     @Autowired
     UserService userService;
 
+    @Operation(summary = "아이디 중복 체크")
     @PostMapping("/userId")
     Boolean isNotDuplicateUserId(@RequestBody AuthUserIdDto authUserIdDto) {
         return userService.isNotDuplicateUserId(authUserIdDto.getUserId());
     }
+    @Operation(summary = "전화번호 중복 체크")
     @PostMapping("/phoneNumber")
     Boolean isNotDuplicatePhoneNumber(@RequestBody AuthPhoneNumberDto authPhoneNumberDto) {
         return userService.isNotDuplicatePhoneNumber(authPhoneNumberDto.getPhoneNumber());
     }
+
+    @Operation(summary = "닉네임 중복 체크")
     @PostMapping("/nickname")
     Boolean isNotDuplicateNickname(@RequestBody AuthNicknameDto authNicknameDto) {
         return userService.isNotDuplicateNickname(authNicknameDto.getNickname());
--- src/main/java/com/scope/socialboardweb/controller/UserRestController.java
+++ src/main/java/com/scope/socialboardweb/controller/UserRestController.java
@@ -4,12 +4,15 @@ import com.scope.socialboardweb.domain.User;
 import com.scope.socialboardweb.dto.*;
 import com.scope.socialboardweb.repository.UserRepository;
 import com.scope.socialboardweb.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name="UserRestController",description = "회원가입, 로그인")
 @RestController
 @RequestMapping("/api/user")
 public class UserRestController {
