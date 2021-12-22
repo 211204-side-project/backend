5957decd321f460fb9b1ab1d1b2b79e5bd3e6611
TaegyunWoo <dnxprbs@gmail.com>
2021-12-22 13:54:51 +0900

    Fix: 테스트코드 빌드 실패 수정
    테스트 시 사용하는 DB 설정을 수정함.

--- src/test/java/com/scope/socialboardweb/repository/custom/CustomUserRepositoryTest.java
+++ src/test/java/com/scope/socialboardweb/repository/custom/CustomUserRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -16,6 +17,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.AUTO_CONFIGURED)
 @ExtendWith(SpringExtension.class)
 @Import(CustomUserRepository.class)
 @DataJpaTest
